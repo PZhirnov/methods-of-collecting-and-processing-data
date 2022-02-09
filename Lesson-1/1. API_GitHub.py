f40b3ee534ca15ef62df708e3b2c99a56294004d
Pavel <pavel-zh@inbox.ru>
2022-02-09 23:36:52 +0300

    Task 1 - API GIT

---
+++ Lesson-1/1. API_GitHub.py
@@ -0,0 +1,126 @@
+"""
+Посмотреть документацию к API GitHub, разобраться как вывести список репозиториев для конкретного пользователя,
+сохранить JSON-вывод в файле *.json; написать функцию, возвращающую(return) список репозиториев.
+"""
+from pprint import pprint
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv('./.env')
+
+
+
+class GitUser:
+    info_user_json = None
+    repo_list_json = None
+    repos_on_all_pages = []
+
+    def __init__(self, username, token=None):
+        self.username = username
+        self.token = token
+
+    def request_data(self, url, param=None):
+        """Возвращает результат запроса по сформированному url"""
+
+        if self.token:
+            info_request = requests.get(url[0], auth=(username, git_token), params=param)
+        else:
+            info_request = requests.get(url[1], params=param)
+        if info_request.status_code == 200:
+            result_json = info_request.json()
+        else:
+            result_json = None
+        return result_json
+
+    def info_user(self):
+        """Возвращает информацию о пользователе"""
+
+        return self.request_data(['https://api.github.com/user',
+                                  f'https://api.github.com/users/{self.username}'])
+
+    def repos_list(self):
+        """Возвращает перечень репозиториев"""
+
+        # Реализована возможность пагинации, чтобы забрать все репозитории
+        page = 1
+        self.repos_on_all_pages = []
+        while True:
+            repos_on_page = self.request_data(['https://api.github.com/user/repos',
+                                               f'https://api.github.com/users/{self.username}/repos'],
+                                              {'page': page, 'per_page': 50})
+            page += 1
+            # если вернулось 0 записей, то можно завершать сбор данных
+            if len(repos_on_page) == 0:
+                break
+            self.repos_on_all_pages.extend(repos_on_page)
+        return self.repos_on_all_pages
+
+    def save_json(self, file_name=None, type_data='info'):
+        """Возвращает перечень репозиториев"""
+
+        if file_name is None:
+            return None
+        if type_data == 'repos':
+            all_repos = self.repos_list() if not len(self.repos_on_all_pages) else self.repos_on_all_pages
+            if all_repos is None:
+                return 'Запрос завершен с ошибкой'
+            result = {}
+            # В файл сохраняем только те поля, которые были по условию
+            for repo in all_repos:
+                result[repo['name']] = repo['html_url']
+        else:
+            result = self.info_user()
+            if result is None:
+                return 'Запрос завершен с ошибкой'
+        try:
+            with open(file_name, 'w', encoding='utf8') as f:
+                json.dump(result, f, indent=2)
+        except FileNotFoundError:
+            return 'Файл не был записан'
+        else:
+            return f'Файл {file_name} создан успешно!'
+
+
+# ----- Проверка работоспособности -------:
+
+# Получим пользователя и токен из
+username = os.getenv('USER_GIT', None)
+git_token = os.getenv('TOKEN_GIT', None)
+
+# Если нужно получить данные по пользователю, для которого известен токен.
+f = GitUser(username, git_token)
+print(f.save_json(f'{username} with token_repos.json', 'repos'))
+print(f.save_json(f'{username} with token_info.json', 'info'))
+
+# Если нужно получить данные по пользователю без токена - только публичные репозитории
+f = GitUser(username)
+print(f.save_json(f'{username} no token_repos.json', 'repos'))
+print(f.save_json(f'{username} no token_info.json', 'info'))
+
+# Пример получения репозиториев Microsoft - 608 ссылок - см. результат выгрузки в отд.
+# f = GitUser('MicrosoftLearning')
+# print(f.save_json(r'MicrosoftLearning_repos.json', 'repos'))
+# print(f.save_json(r'MicrosoftLearning_info.json', 'info'))
+
+
+#
+# def sss():
+#     # Получим информацию о пользователе
+#     info_user = requests.get('https://api.github.com/user', auth=(username, git_token))
+#     pprint(info_user.json(), indent=1)
+#
+#     # Получим все ропозитории пользователя
+#     all_repos = requests.get('https://api.github.com/user/repos', auth=(username, git_token))
+#     print(all_repos.json()[1])
+#
+#     print('Запрос в лоб', requests.get('https://api.github.com/users/PZhirnov/repos').json())
+#     result = {}
+#     for repo in all_repos.json():
+#         result[repo['name']] = repo['html_url']
+#
+#     print(result)
+#
+#     with open('result.json', 'w') as f:
+#         json.dump(result, f)
