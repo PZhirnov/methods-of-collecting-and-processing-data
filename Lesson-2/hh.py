318fbbea0b0059ac8f7c2b6535a56fb5bf829e84
Pavel <pavel-zh@inbox.ru>
2022-02-14 23:41:43 +0300

    correct

--- Lesson-2/hh.py
+++ Lesson-2/hh.py
@@ -3,31 +3,42 @@ from bs4 import BeautifulSoup
 import re
 import time
 import pandas as pd
+from random import randint
 
 
 URL_MAIN = 'https://hh.ru'
 
 
 def clear(text):
-    text = text.replace(' ', '')
+    text = text.replace(' ', '').replace('\u202f', '')
     result = re.findall(r'\d+', text)
-    return result[0]
+    return result
 
 
 def compensation(compensation_val):
-    new_list = []
-    if 'до' in compensation_val:
-        new_list = [0, clear(compensation_val)]
-
-    if 'от' in compensation_val:
-        new_list = [clear(compensation_val), None]
-
-    if '-' in compensation_val:
-        new_list = compensation_val.split('-')
-        new_list = list(map(lambda x: clear(x), new_list))
-    # Получим валюту
-    currency = compensation_val.split(' ')[-1]
-    new_list.append(currency)
+    """
+    Обработка данных о заработной плате
+    :param compensation_val:
+    :return: список - [ от, до, валюта ]
+    """
+    val_from = None
+    val_to = None
+    currency = None
+    new_list = [val_from, val_to, currency]
+    try:
+        val_range = clear(compensation_val)
+        print(compensation_val)
+        if 'до' in compensation_val:
+            val_to = val_range[0]
+        if 'от' in compensation_val:
+            val_from = val_range[0]
+        if len(val_range) == 2:
+            val_from, val_to = val_range
+        # Получим валюту
+        currency = compensation_val.split(' ')[-1]
+        new_list = [val_from, val_to, currency]
+    except ValueError:
+        pass
     return new_list
 
 
@@ -36,16 +47,18 @@ def request_page(url_page):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                       "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
     }
-    result_html = requests.get(url_page, headers=headers).text
-    return result_html
+    result_html = requests.get(url_page, headers=headers)
+    print(result_html.status_code)
+    print(result_html.text)
+    return result_html.text
 
 
 class DataFromPage:
     num_vacancies_on_page = 0
     page_next_href = None
+    soup_page_full = None
 
     def __init__(self, html_page):
-        self.html_page = html_page
         self.soup_page_full = BeautifulSoup(html_page, "lxml")
         # Получим полный перечень вакансий на странице
         self.vacancy_items = self.soup_page_full.find_all('div',
@@ -61,40 +74,40 @@ class DataFromPage:
         else:
             self.page_next_href = None
 
-    def get_item(self, index_vacancy):
+    def get_item(self, vacancy):
         # Получаем все данные по вакансии
-        item = self.vacancy_items[index_vacancy]
+        item = vacancy
         # 1. Наименование вакансии и ссылка
         vacancy_title = item.a.string
         vacancy_href = item.a.attrs.get('href')
-        # 2. Условия оплаты
+        # 2. Условия оплаты - диапазон от и до, валюта.
         vacancy_compensation = item.find('span', attrs={'data-qa': 'vacancy-serp__vacancy-compensation'})
         if vacancy_compensation:
-            compensation_range = vacancy_compensation.get_text()  # диапазон от и до
+            salary_from, salary_to, salary_currency = compensation(vacancy_compensation.text)
         else:
-            compensation_range = "n/a"
+            salary_from, salary_to, salary_currency = [None, None, None]
         # 3. Работодатель и ссылка на него
         vacancy_employer = item.find('div', attrs={'class': 'vacancy-serp-item__info'})
         company_employer = vacancy_employer.next
         company_employer_a = company_employer.find_all('a')
         # вывод для 3
         if company_employer_a:
-            name_employer_name = company_employer_a[0].text.replace('\xa0', ' ')
-            name_employer_href = URL_MAIN + company_employer_a[0].attrs.get('href')
+            employer_name = company_employer_a[0].text.replace('\xa0', ' ')
+            employer_href = URL_MAIN + company_employer_a[0].attrs.get('href')
         else:
-            name_employer_name = 'n/a'
-            name_employer_href = 'n/a'
+            employer_name = None
+            employer_href = None
 
         # 4. Статус работодателя
-        if company_employer_a:
+        try:
             status_link = company_employer_a[1].attrs.get('href')
             status_class = company_employer_a[1].next.get('class')[0]
-        else:
+        except IndexError:
             status_link = None
             status_class = None
         # 5. Адрес
         address_employer_html = item.find('div', attrs={'data-qa': 'vacancy-serp__vacancy-address'})
-        address_employer = ' '.join(address_employer_html.text.split(','))  # вывод Москва - Шаболовка
+        address_employer = ', '.join(address_employer_html.text.split(','))  # вывод Москва - Шаболовка
 
         # 6. Получаем описание вакансии
         g_user_content = item.find('div', attrs={'class': 'g-user-content'})
@@ -107,8 +120,11 @@ class DataFromPage:
             responsibility, requirements = [None, None]
         return [vacancy_title,
                 vacancy_href,
-                compensation_range,
-                name_employer_name,
+                salary_from,
+                salary_to,
+                salary_currency,
+                employer_name,
+                employer_href,
                 status_link,
                 status_class,
                 address_employer,
@@ -119,13 +135,13 @@ class DataFromPage:
     def start_parsing(self, page_num=None):
         """
         Запуск процедуры сбора данных
-        :param num_pages:
+        :param page_num: обозначение страницы - для вывода
         :return: список списков со всеми вакансиями страницы
         """
         result_list = []
         i = 0
         for vacancy in self.vacancy_items:
-            vacancy_data = self.get_item(i)
+            vacancy_data = self.get_item(vacancy)
             result_list.append(vacancy_data)
             i += 1
             print(f'{page_num} - {str(i)}:', vacancy_data)
@@ -149,13 +165,24 @@ def get_hh(url_start, page_num=None):
             page_html = request_page(vac_on_page.page_next_href)
         else:
             break
-        time.sleep(5)
+        time.sleep(randint(3, 10))
     return data_all_pages
 
 
 START_URL = 'https://hh.ru/search/vacancy?area=1&fromSearchLine=true&text=Python'
 
-data = get_hh(START_URL, 1)
-data_frame = pd.DataFrame(data)
+data = get_hh(START_URL, 1)  # если добавить параметр, то можно установить количество выгружаемых страниц
+data_frame = pd.DataFrame(data, columns=['Наименование вакансии',
+                                         'Ссылка на вакансию',
+                                         'ЗП от',
+                                         'ЗП до',
+                                         'Валюта ЗП',
+                                         'Работодатель',
+                                         'Ссылка на работодателя',
+                                         'Описание статуса',
+                                         'Статус проверки работодателя',
+                                         'Адрес',
+                                         'Обзанности',
+                                         'Требования',
+                                         ])
 data_frame.to_excel("output.xlsx")
-
